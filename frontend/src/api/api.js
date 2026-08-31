7ffc6d62f65b8487d096867ccd2b046984760e7a
tekweb26 <tekweb26@gmail.com>
2026-08-31 14:34:55 +0530

    update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://kytp-backend.onrender.com//api";
+  "https://kytp-backend.onrender.com/api";
 
 
 /* =====================================================
